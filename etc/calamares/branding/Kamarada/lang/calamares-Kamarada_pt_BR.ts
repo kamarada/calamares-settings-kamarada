3c5671ddde8e5d9ab29f876869e1073883b41e86
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2020-02-07 18:21:50 -0300

    Small fixes

--- etc/calamares/branding/Kamarada/lang/calamares-Kamarada_pt_BR.ts
+++ etc/calamares/branding/Kamarada/lang/calamares-Kamarada_pt_BR.ts
@@ -4,7 +4,7 @@
 <context>
     <name>show</name>
     <message>
-        <location filename="../Kamarada/show.qml" line="42"/>
+        <location filename="../show.qml" line="43"/>
         <source>Welcome to Debian GNU/Linux.&lt;br/&gt;The rest of the installation is automated and should complete in a few minutes.</source>
         <translation>BR Welcome to Debian GNU/Linux.&lt;br/&gt;BR The rest of the installation is automated and should complete in a few minutes.</translation>
     </message>
